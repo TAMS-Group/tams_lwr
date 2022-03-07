9b98176ba3bfb3739d28b836eab4a1bd3f2866d8
Hongzhuo Liang <lianghongzhuo@126.com>
2022-03-07 21:05:23 +0100

    format

--- ros_fri/scripts/debug_external_wrench.sh
+++ ros_fri/scripts/debug_external_wrench.sh
@@ -4,7 +4,6 @@ rosrun rqt_plot rqt_plot \
 /lwr/estimatedExternalWrench/wrench/force/x \
 /lwr/estimatedExternalWrench/wrench/force/y \
 /lwr/estimatedExternalWrench/wrench/force/z \
-\
 /lwr/estimatedExternalTcpWrench/wrench/force/x \
 /lwr/estimatedExternalTcpWrench/wrench/force/y \
 /lwr/estimatedExternalTcpWrench/wrench/force/z \
--- ros_fri/scripts/plot_external_wrench.sh
+++ ros_fri/scripts/plot_external_wrench.sh
@@ -1,5 +1,4 @@
 # plot current measured joint_state efforts on the LWR arm
-#
 rosrun rqt_plot rqt_plot \
 /lwr/estimatedExternalWrench/wrench/force/x \
 /lwr/estimatedExternalWrench/wrench/force/y \
